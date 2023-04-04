5d9ad4f1abf6c3943dd27ab40505b4cf53b8230f
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-04-04 19:22:05 -0400

    rolled back small addition padding and relaxed base joint limits

--- scripts/radar_start_goal.py
+++ scripts/radar_start_goal.py
@@ -10,7 +10,7 @@ radar_start = np.tile(q, (num_start*goal_per_start, 1))
 for i in range(num_start):
     for j in range(goal_per_start):
         radar_start[i*goal_per_start + j, 0] = (i*(2*np.pi))/num_start
-        radar_start[i*goal_per_start + j, 0] -= (np.pi - 0.01) # bringing to -pi to pi
+        radar_start[i*goal_per_start + j, 0] -= np.pi # bringing to -pi to pi
 
 radar_start_file = '../examples/manipulation/resources/shield/radar_starts.txt'
 with open(radar_start_file, "w") as f:
